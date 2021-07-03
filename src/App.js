283d29a791aedaf8455d28b9f1fdcb7c3be260a9
Parth Arora <84407149+parth270@users.noreply.github.com>
2021-07-03 21:33:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useEffect, useCallback } from "react";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import { Route, Switch } from "react-router-dom";
+import Basket from "./components/basket/Basket";
+import Login from "./components/login/Login";
+import { useDispatch } from "react-redux";
+import { itemActions } from "./store/item";
+import Checkout from "./components/basket/Checkout";
+import { Elements } from "@stripe/react-stripe-js";
+import {loadStripe } from '@stripe/stripe-js';
+import Order from "./components/basket/Order";
+
+const promise = loadStripe(
+  "pk_test_51J8OruSAJSO5Ac0I7wRusrFqDkFlubTKFZm8B2jodKpomd0rCO6a8xp60zdwjt5XVPjjWlFORxsD9030AHV3Zcb300BiHPoXfF"
+);
 
 function App() {
+  const dispatch = useDispatch();
+
+  const sendHandler = useCallback(() => {
+    dispatch(itemActions.clearHandler());
+
+    fetch("https://clone-8e97f-default-rtdb.firebaseio.com/items.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const loadeditems = [];
+
+        const newData = Object.values(data);
+
+        for (const i in newData) {
+          const newerData = Object.values(newData[i]);
+          for (const key in newerData) {
+            const item = {
+              amount: newerData[key].amount,
+              id: newerData[key].id,
+              image: newerData[key].image,
+              quantity: newerData[key].quantity,
+              star: newerData[key].star,
+              title: newerData[key].title,
+            };
+            loadeditems.push(item);
+            dispatch(itemActions.addHandler(item));
+          }
+        }
+      });
+  }, [dispatch]);
+
+  useEffect(() => {
+    sendHandler();
+  }, [sendHandler]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Switch>
+        <Route path="/" exact>
+          <Header />
+          <Home />
+        </Route>
+        {!localStorage.getItem("status") && (
+          <Route path="/login">
+            <Login />
+          </Route>
+        )}
+        <Route path="/basket" exact>
+          <Header />
+          <Basket />
+        </Route>
+        <Route path="/orders">
+        <Header />
+          <Order/>
+        </Route>
+        <Route path="/basket/checkout">
+          <Header />
+          <Elements stripe={promise}>
+            <Checkout />
+          </Elements>
+        </Route>
+        <Route path="*">
+          <div className="error-container">
+            <h1>404 some error occured</h1>
+            <p className="center">(some reason) </p>
+          </div>
+        </Route>
+      </Switch>
+    </Fragment>
   );
 }
 
 export default App;
+
+// database:-https://clone-8e97f-default-rtdb.firebaseio.com/
+
