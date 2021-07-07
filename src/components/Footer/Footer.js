c8b20ef52af43b5bb0508e8ca6d9d6ad79f0790e
Parth Arora <84407149+parth270@users.noreply.github.com>
2021-07-08 03:27:53 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Footer.module.css';
+
+const Footer=()=>{
+    return(
+        <div className={classes.container} >
+            <p>Amazon-clone by Parth Arora</p>
+        </div>
+    );
+}
+
+export default Footer;
